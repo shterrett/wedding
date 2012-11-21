df61e9ee3be644723c774a37da3938ff54e06ea8
Stuart Terrett <sterrett@mp-sportsclub.com>
2012-11-21 13:37:07 -0500

    Fixes home page javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
 
 function dynamicAddRemove(parent, parents, child, children) {
   $('a.' + child + '-add').live('click', function() {
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,40 +1,39 @@
-// for home page
-// function hideAll(){
-// 	$("section").hide();
-// }
-// 
-// function showWhen() {
-// 	hideAll();
-// 	hideImage();
-// 	$("#when").slideDown('slow');
-// }
-// 
-// function showWhere() {
-// 	hideAll();
-// 	hideImage();
-// 	$("#where").slideDown('slow');
-// }
-// 
-// function showHow(){
-// 	hideAll();
-// 	hideImage();
-// 	$("#how").slideDown('slow');
-// }
-// 
-// function showRegistry(){
-// 	hideAll();
-// 	hideImage();
-// 	$("#registry").slideDown('slow');
-// }
-// 
-// function hideImage(){
-// 	$("#initial").fadeOut('fast');
-// }
-// 
-// $().ready( function() {
-// 	hideAll();
-// 	$("#when-link").click(showWhen());
-// 	$("#where-link").click(showWhere());
-// 	$("#how-link").click(showHow());
-// 	$("#registry-link").click(showRegistry());
-// });
+function hideAll(){
+	$("section").hide();
+}
+
+function showWhen() {
+	hideAll();
+	hideImage();
+	$("#when").slideDown('slow');
+}
+
+function showWhere() {
+	hideAll();
+	hideImage();
+	$("#where").slideDown('slow');
+}
+
+function showHow(){
+	hideAll();
+	hideImage();
+	$("#how").slideDown('slow');
+}
+
+function showRegistry(){
+	hideAll();
+	hideImage();
+	$("#registry").slideDown('slow');
+}
+
+function hideImage(){
+	$("#initial").fadeOut('fast');
+}
+
+$().ready( function() {
+	hideAll();
+	$("#when-link").click(function() { showWhen() });
+	$("#where-link").click(function() { showWhere() });
+	$("#how-link").click(function() { showHow() });
+	$("#registry-link").click(function() { showRegistry() });
+});
