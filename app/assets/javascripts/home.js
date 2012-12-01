fb88b2d26691df3760f021be3f8ea9cc1f5be0a3
Stuart <shterrett@gmail.com>
2012-12-01 10:43:49 -0500

    Refactors javascript for home page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,39 +2,17 @@ function hideAll(){
 	$("section").hide();
 }
 
-function showWhen() {
-	hideAll();
-	hideImage();
-	$("#when").slideDown('slow');
-}
-
-function showWhere() {
-	hideAll();
-	hideImage();
-	$("#where").slideDown('slow');
-}
-
-function showHow(){
-	hideAll();
-	hideImage();
-	$("#how").slideDown('slow');
-}
-
-function showRegistry(){
-	hideAll();
-	hideImage();
-	$("#registry").slideDown('slow');
-}
-
 function hideImage(){
 	$("#initial").fadeOut('fast');
 }
 
 $().ready( function() {
 	hideAll();
-	$(".flash").fadeIn('fast')
-	$("#when-link").click(function() { showWhen() });
-	$("#where-link").click(function() { showWhere() });
-	$("#how-link").click(function() { showHow() });
-	$("#registry-link").click(function() { showRegistry() });
+	$(".main-nav>a").click(function(e){
+		e.preventDefault();
+		hideImage();
+		hideAll();
+		var section_id = "#" + $(this).attr('id').replace('-link', '');
+		$(section_id).slideDown('slow').addClass("active");		
+	});
 });
