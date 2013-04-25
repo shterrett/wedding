76728da56fac74a943c467067b540adb9e4149a1
Stuart Terrett <shterrett@gmail.com>
2013-04-25 06:28:16 -0400

    ContactForm tests

--- spec/models/contact_form_spec.rb
+++ spec/models/contact_form_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe ContactForm do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:contact_form) { ContactForm.new }
+  subject { contact_form }
+  
+  [:name, :email, :body].each do |attr|
+    it { should respond_to attr }
+  end
+  
+  it "should validate name, email, and body" do
+    contact_form.should_not be_valid
+    contact_form.name = "Stuart"
+    contact_form.email = "stuart@example.com"
+    contact_form.body = "Test"
+    contact_form.should be_valid
+  end
+  
+  it "should send email if valid" do
+    pending
+  end
+  
 end
