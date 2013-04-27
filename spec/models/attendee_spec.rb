a3b38065c15555b0066958d5e7dbfe73893b8803
Stuart Terrett <shterrett@gmail.com>
2013-04-27 10:32:01 -0400

    Attendee tests

--- spec/models/attendee_spec.rb
+++ spec/models/attendee_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Attendee do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:attendee) { Attendee.new }
+  subject { attendee }
+  
+  [:name, :email, :vegetarian, :attending, :message].each do |attr|
+    it { should respond_to attr }
+  end
+  
+  it "should belong_to invitation" do
+    inv = Invitation.create
+    inv.attendees << attendee
+    attendee.invitation.should == inv
+  end
+  
+  
 end
--- spec/models/contact_form_spec.rb
+++ spec/models/contact_form_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe ContactForm do
   let(:contact_form) { ContactForm.new }
+  let(:valid_contact_form) do
+    ContactForm.new(name: "Stuart", email: "stuart@example.com", body: "Test")
+  end
+    
+  
   subject { contact_form }
   
   [:name, :email, :body].each do |attr|
@@ -10,14 +15,7 @@ describe ContactForm do
   
   it "should validate name, email, and body" do
     contact_form.should_not be_valid
-    contact_form.name = "Stuart"
-    contact_form.email = "stuart@example.com"
-    contact_form.body = "Test"
-    contact_form.should be_valid
-  end
-  
-  it "should send email if valid" do
-    pending
+    valid_contact_form.should be_valid
   end
   
 end
