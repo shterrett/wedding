d931595a94ba522789fc43ba5a4690bc76b188a4
Stuart Terrett <shterrett@gmail.com>
2013-05-01 06:28:09 -0400

    finish tests for /admin/invitations

--- spec/features/admin_invitation_spec.rb
+++ spec/features/admin_invitation_spec.rb
@@ -1,18 +1,32 @@
 require 'spec_helper'
 
 feature "admin invitations views" do
-  before(:all) { 30.times { FactoryGirl.create :invitation } }
+  before(:all) do
+    30.times do 
+      FactoryGirl.create :invitation
+    end
+  end 
   let(:num) { Random.rand(0...30) }
   after(:all) { Invitation.delete_all }
+  
   scenario "Visit Invitation Index" do
-    num
     visit '/admin/invitations'
+    num
     expect(page).to have_text "All Invitations"
     expect(page).to have_link "Stuart_#{num}"
   end
-  scenario "click into invitation" do
-    num
+  scenario "Search list" do
     visit '/admin/invitations'
+    num
+    other_num = num - 1
+    fill_in "search", with: "Stuart_#{num}"
+    click_button "Search"
+    page.should have_link "Stuart_#{num}"
+    page.should_not have_link "Stuart_#{other_num}"
+  end
+  scenario "click into invitation" do
+    visit '/admin/invitations'
+    num
     click_link "Stuart_#{num}"
     expect(page).to have_text "Stuart_#{num}"
   end
