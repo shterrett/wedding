4a350b79a74a5e05af3d26b95d6d1ec2e4b403c1
Stuart Terrett <shterrett@gmail.com>
2013-05-10 06:57:09 -0400

    Finishes basic Invitations controller and views. Need to add nested form for attendees in new/edit and attendees attributes in show.

--- app/controllers/admin/invitations_controller.rb
+++ app/controllers/admin/invitations_controller.rb
@@ -1,5 +1,35 @@
 class Admin::InvitationsController < ApplicationController
   
+  def new
+    @invitation = Invitation.new
+  end
+  
+  def create
+    @invitation = Invitation.new(params[:invitation])
+    if @invitation.save
+      flash[:success] = "Invitation Successfully Created"
+      redirect_to new_admin_invitation_path
+    else
+      flash[:error] = "Error in Invitation; please review"
+      render 'new'
+    end
+  end
+  
+  def edit
+    @invitation = Invitation.find(params[:id])
+  end
+  
+  def update
+    @invitation = Invitation.find(params[:id])
+    if @invitation.update_attributes(params[:invitation])
+      flash[:success] = "Invitation Successfully Updated"
+      redirect_to admin_invitation_path(@invitation)
+    else
+      flash[:error] = "Error in Invitation; please review"
+      render 'edit'
+    end
+  end
+  
   def index
     @invitations = Invitation.all
   end
--- spec/features/admin_invitation_spec.rb
+++ spec/features/admin_invitation_spec.rb
@@ -6,10 +6,10 @@ feature "admin invitations views" do
       FactoryGirl.create :invitation
     end
   end 
-  let(:num) { Random.rand(0...30) }
+  let(:num) { Random.rand(1...30) }
   after(:all) { Invitation.delete_all }
   
-  scenario "Visit Invitation Index" do
+  scenario "Visit Invitation#index" do
     visit '/admin/invitations'
     num
     expect(page).to have_text "All Invitations"
@@ -29,5 +29,58 @@ feature "admin invitations views" do
     num
     click_link "Stuart_#{num}"
     expect(page).to have_text "Stuart_#{num}"
+  end
+end
+
+feature "admin create invitations" do
+  scenario "Visit Invitation#new" do
+    visit '/admin/invitations/new'
+    expect(page).to have_text "Create an Invitation"
+    expect(page).to have_field "Principal Name"
+  end
+  
+  scenario "create a new invitation" do
+    visit '/admin/invitations/new'
+    fill_in "Honorific", with: "Mr."
+    fill_in "Principal Name", with: "Stuart Terrett"
+    fill_in "Street Address", with: "123 Test Rd."
+    fill_in "City", with: "Savannah"
+    fill_in "State", with: "GA"
+    fill_in "Zip", with: 31405
+    expect do
+      click_button "Create Invitation"
+    end.to change(Invitation, :count).by(1)
+  end
+    
+end
+
+feature "admin edit invitations" do
+  scenario "Visit Invitation#edit" do
+    invitation = FactoryGirl.create :invitation
+    visit "/admin/invitations/#{invitation.id}/edit"
+    expect(page).to have_text "Edit Invitation for #{invitation.principal_name}"
+    expect(page).to have_field "Principal Name"
+  end
+  
+  scenario "update invitation" do
+    invitation = FactoryGirl.create :invitation
+    id = invitation.id
+    visit "/admin/invitations/#{invitation.id}/edit"
+    fill_in "Principal Name", with: "Not in the pattern"
+    expect do
+      click_button "Update Invitation"
+      invitation.reload
+    end.to change(invitation, :principal_name).to("Not in the pattern")
+  end
+end
+
+feature "admin view invitation" do
+  scenario "Visit Invitation#show" do
+    invitation = FactoryGirl.create :invitation
+    visit "/admin/invitations/#{invitation.id}"
+    expect(page).to have_text invitation.principal_name
+    expect(page).to have_text invitation.street_address
+    expect(page).to have_link("Edit", { href: "/admin/invitations/#{invitation.id}/edit" })
+    expect(page).to have_link("Back to Index", { href: "/admin/invitations" })
   end
 end
