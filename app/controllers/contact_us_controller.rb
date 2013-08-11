901f54983fbfd8f032a042881f3ba553544747ea
Stuart Terrett <shterrett@gmail.com>
2013-08-11 09:29:39 -0400

    Disables contact form

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -7,11 +7,13 @@ class ContactUsController < ApplicationController
   def create
     @contact_form = ContactForm.new params[:contact_form]
     if @contact_form.valid?
-      RsvpMailer.contact_us(@contact_form).deliver
-      redirect_to root_path, notice: "Email sent successfully"
+      # RsvpMailer.contact_us(@contact_form).deliver
+      # redirect_to root_path, notice: "Email sent successfully"
     else
-      render 'new'
+      # render 'new'
     end
+    flash[:error] = "Contact form has been disabled."
+    redirect_to root_path
   end
   
 end
